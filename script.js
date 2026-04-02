47c14ccd1764bf6cbf6d4205a0e5dfb2a8ee6c3f
Binay Mann <82080713+Binay-tech1608@users.noreply.github.com>
2026-04-02 08:06:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function daysOfAYear(year){
+	let num = parseInt(year);
+	//First we check if the mentioned year is a leap year.
+	if(num%400==0 && num%4==0 && num>=400){
+		return 366;
+	}
+	else{
+		return 365;
+	}
+}
